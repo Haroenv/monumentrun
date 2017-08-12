68114148170e6c0623e6f2ec8d9698ac8331c813
Haroen Viaene <hello@haroen.me>
2017-08-12 22:11:09 +0200

    Typing

--- helpers/location.js
+++ helpers/location.js
@@ -1,24 +1,45 @@
-export const toLatLng = stop => ({ longitude: stop[0], latitude: stop[1] });
+type LatLng = { longitude: number, latitude: number };
+type MaybeLatLng = LatLng | null;
 
-export function getCenter({ here, venues, history }) {
-  function getLatLng({ here, venues, history }) {
-    if (here) {
-      return;
-      here;
-    }
-    if (venues.length > 0) {
-      return {
-        longitude: venues[0].location.lng,
-        latitude: venues[0].location.lat,
-      };
-    }
-    if (history && history.length > 0) {
-      return {
-        ...history[0],
-      };
-    }
-    return null;
+export const toLatLng = (stop: [number, number]) => ({
+  longitude: stop[0],
+  latitude: stop[1],
+});
+
+function getLatLng({
+  here,
+  venues,
+  history,
+}: {
+  here: LatLng,
+  venues: Array<{ location: { lng: number, lat: number } }>,
+  history: Array<LatLng>,
+}): MaybeLatLng {
+  if (here) {
+    return here;
   }
+  if (venues.length > 0) {
+    return {
+      longitude: venues[0].location.lng,
+      latitude: venues[0].location.lat,
+    };
+  }
+  if (history && history.length > 0) {
+    return {
+      ...history[0],
+    };
+  }
+  return null;
+}
+
+type Center = {
+  ...LatLng,
+  latitudeDelta: number,
+  longitudeDelta: number,
+};
+type Nothing = {||};
+
+export function getCenter({ here, venues, history }): Nothing | Center {
   const latLng = getLatLng({ here, venues, history });
   if (latLng === null) {
     return {};
